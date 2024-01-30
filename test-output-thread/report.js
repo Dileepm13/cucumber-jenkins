35dbdf88067c830be92271332759cd157b595cf5
Dileep <dileepemail@gmail.com>
2024-01-30 23:46:30 +0530

    done

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"8f84cdf9-2bbd-44e6-8ee6-034eebf78c9d","feature":"Register multiple users on the Rediff account Page","scenario":"Test rediff Register Account Page","start":1706636237425,"group":1,"content":"","tags":"","end":1706636260837,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[#1,main,5,main]"}]);
+});
