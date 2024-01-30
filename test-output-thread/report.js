68d3538be837e75c0d6b801745ffdf8ae1bdf7c1
Dileep <dileepemail@gmail.com>
2024-01-31 00:49:43 +0530

    lombak

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"8f84cdf9-2bbd-44e6-8ee6-034eebf78c9d","feature":"Register multiple users on the Rediff account Page","scenario":"Test rediff Register Account Page","start":1706636237425,"group":1,"content":"","tags":"","end":1706636260837,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"0b67eff0-54da-4a2d-8003-89067461fdf0","feature":"Register multiple users on the Rediff account Page","scenario":"Test rediff Register Account Page","start":1706642279828,"group":1,"content":"","tags":"","end":1706642305510,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[#1,main,5,main]"}]);
 });
